722b8f6edf8280109927759157e14f95f86488b9
MK-MF <52254788+wosshitianming@users.noreply.github.com>
2025-02-24 22:22:12 +0800

    Update notability.js

--- notability.js
+++ notability.js
@@ -1,33 +1,37 @@
-let obj = JSON.parse($response.body);
-obj = {
-	"data": {
-		"processAppleReceipt": {
-			"__typename": "SubscriptionResult",
-			"isClassic": false,
-			"subscription": {
-				"productId": "com.gingerlabs.Notability.premium_subscription",
-				"originalTransactionId": "90002027553673",
-				"tier": "premium",
-				"refundedDate": null,
-				"refundedReason": null,
-				"isInBillingRetryPeriod": false,
-				"expirationDate": "2999-12-31T24:00:00.000Z",
-				"gracePeriodExpiresAt": null,
-				"overDeviceLimit": false,
-				"expirationIntent": "CUSTOMER_RENEWAL",
-				"__typename": "AppStoreSubscription",
-				"user": {
-					"id": "0b9c2a38-9d7c-4659-9812-623af642d392",
-					"email": "",
-					"__typename": "User"
-				},
-				"status": "active",
-				"originalPurchaseDate": "2024-06-16T18:30:45.000Z"
-			},
-			"error": 0
-		}
-	}
-};
-$done({
-	body: JSON.stringify(obj)
-});
+/*************************************
+
+项目名称：Notability:笔记,PDF
+下载地址：https://t.cn/A6Cgjtei
+更新日期：2025-02-24
+脚本作者：
+电报频道：
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Notability.js
+
+[mitm]
+hostname = notability.com
+
+*************************************/
+
+var encode_version = 'jsjiami.com.v5', aabeh = '__0x124601',  __0x124601=['X3MMaVB2w5ZeXMO2wosjw4TDsQ==','CcK6EsKL','Y8Oww6I=','wqY0Gg==','wrzCmsOrw78=','4puS77i0w5johaXmnpLlv7Tlu6/ltYznu5nmrZbovpboo4c=','5qOD5raW5YuV6Ia15p+t5b6y5Yaq5p+h5b6i5ZKl','8LOQtQDjgb99wqArwoEvXumHt+e9jeaOq+WPreOBusKCwqrvu7TigafCiOmHiue/jOWeseWeje+/qMO2c8Oywqtlwq/DhMK4MBFMP8OOw77CksKow6fDo3HDpcKRw4wmRwN8JRNxwpfDi8Kpw6TCsUbDiMOvIsKnw6bvuq3igJDCiuivhemYmemRhuaNj++8vW7Djx5iw5IQw59Iw7Y7wrdqw53Dsh9AJMKpwrwkPMOZwobDv8Ofw5jCusOkw4cQw5fDl8K1Y0vDsMKRwrFTOsOGw5t8VcK4w4fCmATCscK6VB7DsAjDjMKUwp7DpMOOfiMhw6R7DWsiXsKyw7csORXCncOIel3ClvCYo7PCoeOCj+S/teeXluivpuaYlOOAnMOKw67vu6jig71v5reh5Yum6K2F6ZmJ6ZGI5o+B5YiKCMKsw4rDsMOfwrhMwp7vuoDig6QW5ZC+55aAEF/ohpvmn7LlvZflhrEvw7Dlu4nkvY/lrKDor7znvLcsHuKZhu+7tsOC44Cw5rKI5oez5LmA6aOO44Khw7rClcK65byi5YWg55aT5Lqd6Ziw5qy66Z245rKd5ZeB5Y2Y6Iav5p+IbztA5Lmn5L2s5a+A5Lqz5Lyb6aiu77yo6K2h5YqO5L6e5pOb5omG5rmX55etw4ELw5LluZDorIcowrjCh8OM5bKd5peU5YeQ5YuQ6Zun77yM6YC65YeT5Lif5b2x6Kah6ZSP6aKvcsKd8J2LogjmhpHosrTnk4DopIrkuJ7mlpfmjZLvvZw=','YMKywojCgcKmw5vCt8Ksw4fDuMKBBFbClmLCokI+F8KYakrDrDfCrA3CpsODwozCkiUbGQfChCbDumJ0wpo3G3vDgMOgwqs=','S8KPCynDolHClsKnwpwaw6hTw5Eiwro=','S8OGSGTCnkMA','L2Asw7V0w5jDmXjDvAtlfhPChWR4wofDnivDk0XCk8OAw5E=','QTDDsiXCjRZuwqNLcCLCoUDChcO5JQ/DiBHCmA==','E1jCjBbDiMKT','w7/CjcKSIMOsKVfCtsK+UnbChFDCuUjCgm7CgA==','AQZa','MsKQw4zCm0bChcOQIcKC','UWQLLlBrw48vTMOuwo95w5HCrA==','w7rCv28=','EMKwMjLDtFPCiyk=','wqXCuMOs','KcONWEjCnA==','eifCsw==','4puP77mAwpXorrTlja7DlHnCjcKfOMOuw7Tphrbnv7vlp4LotIrvvY0=','BRQSwpbDu1dl','w5XCi1Q=','dCPCpiM/','w5gzEiLDvg==','w6HCoFIMXg==','T2UCN0E=','ZBx9wpkP','wpvCuMOpcg==','esOrw6jDuMOq','8KSmjArmiZPli7zor67ljYXCksK4wo06wqrDgMOX5YK6776Nw4DCilZZLMKSW0l8JQ89w4TDigop77yJ77yt','HcKZCQ==','w4/CkQhdHQ==','PgrDqcKXGR7Co3M=','8JGVuMOr5omW5YuZ6K6+5Y2Qw7lfWsKNSUQF5YCI772bbsOKwok7QUjvv77vvqQ=','wozDhcOQ','4pmA77qKw6nml7LmsazmoqLmt5TliYPljoHnl6bnmovDv8OKQMKEGcOVcOeOueWjjO+/ow==','54qF5p695Y+s77+8w4vCiOS+m+Wuj+adsuW/jOeqme++tui8meivjOaUpeaPleaKqOS5jOeanOW3juS+tw==','dwhWwo40','w6jCshfDiRLCpcOuw4PCmA==','csOgw6fCqcOfU1sl','AsOHwpc=','4puT77qGw6fmlKzmsoPmoqzmtoblib7ljLfnla/nmaFCwolFeilYeeeNguWjme+/pQ==','EEbCpl9X','w6HCkS1CDw==','TMORbsKGw68=','VBZC','HjzDtsK+Pw==','JsOQQSEq','wrXDuHxQwp4=','wozDqsKu','8KymksKl5ouY5Yqq6K6o5Y2ewrkKwqt3PFAR5YGz77+aA1JVwpxbwqcUw4c3LVTDmk1Ww4ZT77yy776Q','w6pmXQ==','CVVJw6Jq','wrvCusO5wq/DrQ==','SMOmw6bDgcOXLcOIw7NZw6s1','w7s9JSDDpQ==','GcOgRjYGU8O8w4DCscKHOw==','EnrCtA==','8Ki3pgnmiq3lipLor5bljKjDuMOZwqNBw7HCkMOa5YCq77yXWTV0wprCtcKT77+37762','VjwG','woHCiSE=','4puB77iowqDorYnljYvChALDt0TDqMOrK+mHuue/p+WntOi0mu+/pw=='];(function(_0x4b7dae,_0xdc4845){var _0x2d9258=function(_0x1b2980){while(--_0x1b2980){_0x4b7dae['push'](_0x4b7dae['shift']());}};_0x2d9258(++_0xdc4845);}(__0x124601,0xae));var _0x3934=function(_0x5486a5,_0x3d1798){_0x5486a5=_0x5486a5-0x0;var _0x78cf55=__0x124601[_0x5486a5];if(_0x3934['initialized']===undefined){(function(){var _0x5b3356=typeof window!=='undefined'?window:typeof process==='object'&&typeof require==='function'&&typeof global==='object'?global:this;var _0x6eaeb6='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x5b3356['atob']||(_0x5b3356['atob']=function(_0x3a68c8){var _0x3ec49e=String(_0x3a68c8)['replace'](/=+$/,'');for(var _0x2173a9=0x0,_0x5749f2,_0x238837,_0x378e32=0x0,_0x11f745='';_0x238837=_0x3ec49e['charAt'](_0x378e32++);~_0x238837&&(_0x5749f2=_0x2173a9%0x4?_0x5749f2*0x40+_0x238837:_0x238837,_0x2173a9++%0x4)?_0x11f745+=String['fromCharCode'](0xff&_0x5749f2>>(-0x2*_0x2173a9&0x6)):0x0){_0x238837=_0x6eaeb6['indexOf'](_0x238837);}return _0x11f745;});}());var _0xe8cd0b=function(_0x2389a2,_0x21c886){var _0x35e7e3=[],_0x44f846=0x0,_0x699297,_0x1aeb6a='',_0x51b250='';_0x2389a2=atob(_0x2389a2);for(var _0x8f9fdd=0x0,_0x764bb6=_0x2389a2['length'];_0x8f9fdd<_0x764bb6;_0x8f9fdd++){_0x51b250+='%'+('00'+_0x2389a2['charCodeAt'](_0x8f9fdd)['toString'](0x10))['slice'](-0x2);}_0x2389a2=decodeURIComponent(_0x51b250);for(var _0x2ea203=0x0;_0x2ea203<0x100;_0x2ea203++){_0x35e7e3[_0x2ea203]=_0x2ea203;}for(_0x2ea203=0x0;_0x2ea203<0x100;_0x2ea203++){_0x44f846=(_0x44f846+_0x35e7e3[_0x2ea203]+_0x21c886['charCodeAt'](_0x2ea203%_0x21c886['length']))%0x100;_0x699297=_0x35e7e3[_0x2ea203];_0x35e7e3[_0x2ea203]=_0x35e7e3[_0x44f846];_0x35e7e3[_0x44f846]=_0x699297;}_0x2ea203=0x0;_0x44f846=0x0;for(var _0x4b1477=0x0;_0x4b1477<_0x2389a2['length'];_0x4b1477++){_0x2ea203=(_0x2ea203+0x1)%0x100;_0x44f846=(_0x44f846+_0x35e7e3[_0x2ea203])%0x100;_0x699297=_0x35e7e3[_0x2ea203];_0x35e7e3[_0x2ea203]=_0x35e7e3[_0x44f846];_0x35e7e3[_0x44f846]=_0x699297;_0x1aeb6a+=String['fromCharCode'](_0x2389a2['charCodeAt'](_0x4b1477)^_0x35e7e3[(_0x35e7e3[_0x2ea203]+_0x35e7e3[_0x44f846])%0x100]);}return _0x1aeb6a;};_0x3934['rc4']=_0xe8cd0b;_0x3934['data']={};_0x3934['initialized']=!![];}var _0x1c3ed3=_0x3934['data'][_0x5486a5];if(_0x1c3ed3===undefined){if(_0x3934['once']===undefined){_0x3934['once']=!![];}_0x78cf55=_0x3934['rc4'](_0x78cf55,_0x3d1798);_0x3934['data'][_0x5486a5]=_0x78cf55;}else{_0x78cf55=_0x1c3ed3;}return _0x78cf55;};
+
+var ddm={'data':{'processAppleReceipt':{'error':0x0,'subscription':{'productId':'com.disney.streaming.subscription.plus','originalTransactionId':'490001196350000','tier':'PREMIUM_PLUS','refundedDate':null,'refundedReason':null,'isInBillingRetryPeriod':![],'expirationDate':'2099-09-09T09:09:09.000Z','gracePeriodExpiresAt':null,'overDeviceLimit':![],'expirationIntent':null,'__typename':'AppleInAppSubscription','user':null,'status':'ACTIVE','originalPurchaseDate':'2024-09-09T09:09:09.000Z'},'isClassic':![],'__typename':'ProcessAppleReceiptResponse'}}};
+$done({'body':JSON['stringify'](ddm)};;
+
+(function(_0x586890,_0x578736,_0x367098){
+var _0x340573={'WucQN':'BwA','trcpp':'undefined','LVmey':'jsjiami.com.v5','tOdXj':function _0x20c39f(_0x40cad9,_0x579c29){return _0x40cad9===_0x579c29;},'QYrca':'function','PAcjX':function _0x3d3aed(_0xf064fe,_0x2145f3){return _0xf064fe!==_0x2145f3;},'rEhUV':function _0x1fbe9d(_0x4232c8,_0x369a56){return _0x4232c8===_0x369a56;},'DlbLX':'read','jHBpd':function _0x1ace61(_0x4586ea,_0x223483){return _0x4586ea!==_0x223483;},'hTCgA':function _0x553e24(_0x56e0af,_0xdc8cc2){return _0x56e0af===_0xdc8cc2;},'aQCBm':'删除版本号，js会定期弹窗'};
+_0x367098='al';
+try{
+_0x367098+=_0x3934('0x31','9^r(');
+_0x578736=encode_version;
+if(!(typeof _0x578736!==_0x340573['trcpp']&&_0x578736===_0x340573['LVmey'])){
+_0x586890[_0x367098](_0x340573['aQCBm']);
+}}catch(_0x379ea4){
+_0x586890[_0x367098](_0x340573['aQCBm']);
+}}(window));
+;
+encode_version = 'jsjiami.com.v5';
