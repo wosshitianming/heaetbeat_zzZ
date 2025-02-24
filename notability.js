dcab73c0854225c4b8938cf7c2a373d885367b70
MK-MF <52254788+wosshitianming@users.noreply.github.com>
2025-02-24 22:41:05 +0800

    Update notability.js

--- notability.js
+++ notability.js
@@ -26,7 +26,7 @@ const response = {
         "status": "active",
         "originalPurchaseDate": "2024-09-19T09:27:35.000Z",
         "originalTransactionId": "570001185968888",
-        "expirationDate": "9999-12-31T23:59:59.000Z",
+        "expirationDate": "2999-12-31T23:59:59.000Z",
         "productId": "com.gingerlabs.Notability.premium_subscription",
         "tier": "premium",
         "refundedDate": null,
